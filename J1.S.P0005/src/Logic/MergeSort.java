f8a3fb4347e60d1c9d3e32a33daa97631c106e2b
Quang2k3 <nguyen.quang6633@gmail.com>
2023-09-15 22:20:47 +0700

    update

---
+++ J1.S.P0005/src/Logic/MergeSort.java
@@ -0,0 +1,136 @@
+package Logic;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Quang
+ */
+public class MergeSort {
+
+    int arr[];
+    int tempMergArr[];
+    int length;
+
+    public MergeSort(int length) {
+        this.length = length;
+        this.arr = new int[length];
+        this.tempMergArr = new int[length];
+        generateRandomIntegerOfArray();
+    }
+
+    public void displayArray() {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+        System.out.println();
+    }
+
+    public void generateRandomIntegerOfArray() {
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(arr.length + 1);
+        }
+    }
+
+//    public void sort(boolean check) {
+//        doMergeSort(0, length - 1, check);
+//    }
+//
+//    public void doMergeSort(int lowerIndex, int higherIndex, boolean check) {
+//        if (lowerIndex < higherIndex) {
+//            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+//            doMergeSort(lowerIndex, middle, check);
+//            doMergeSort(middle + 1, higherIndex, check);
+//            mergeParts(lowerIndex, middle, higherIndex, check);
+//        }
+//    }
+//
+//    private void mergeParts(int lowerIndex, int middle, 
+//            int higherIndex, boolean check) {
+//        for (int i = lowerIndex; i <= higherIndex; i++) {
+//            tempMergArr[i] = arr[i];
+//        }
+//        int i = lowerIndex;
+//        int j = middle + 1;
+//        int k = lowerIndex;
+//        while (i <= middle && j <= higherIndex) {
+//            if (check) {
+//                if (tempMergArr[i] <= tempMergArr[j]) {
+//                    arr[k] = tempMergArr[i];
+//                    i++;
+//                } else {
+//                    arr[k] = tempMergArr[j];
+//                    j++;
+//                }
+//            } else {
+//                if (tempMergArr[i] >= tempMergArr[j]) {
+//                    arr[k] = tempMergArr[i];
+//                    i++;
+//                } else {
+//                    arr[k] = tempMergArr[j];
+//                    j++;
+//                }
+//            }
+//            k++;
+//        }
+//        while (i <= middle) {
+//            arr[k] = tempMergArr[i];
+//            k++;
+//            i++;
+//        }
+//    }
+    
+    public void sort(boolean check) {
+        doMergeSort(0, length - 1, check);
+    }
+
+    public void doMergeSort(int lowerIndex, int higherIndex, boolean check) {
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            doMergeSort(lowerIndex, middle, check);
+            doMergeSort(middle + 1, higherIndex, check);
+            mergeParts(lowerIndex, middle, higherIndex, check);
+        }
+    }
+
+    private void mergeParts(int lowerIndex, int middle, int higherIndex, boolean check) {
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = arr[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (check) {
+                if (tempMergArr[i] <= tempMergArr[j]) {
+                    arr[k] = tempMergArr[i];
+                    i++;
+                } else {
+                    arr[k] = tempMergArr[j];
+                    j++;
+                }
+                k++;
+            } else {
+                if (tempMergArr[i] >= tempMergArr[j]) {
+                    arr[k] = tempMergArr[i];
+                    i++;
+                } else {
+                    arr[k] = tempMergArr[j];
+                    j++;
+                }
+                k++;
+            }
+        }
+        while (i <= middle) {
+            arr[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+    }
+}
--- J1.S.P0005/src/Main/Main.java
+++ J1.S.P0005/src/Main/Main.java
@@ -1,7 +1,7 @@
 package Main;
 
-import Validate.MergeSort;
-import java.util.Scanner;
+import Logic.MergeSort;
+import static Validate.Validate.getInt;
 
 /**
  *
@@ -9,51 +9,16 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static final Scanner SCANNER = new Scanner(System.in);
-
-    /**
-     * Returns the valid integer value scanner from the input
-     *
-     * @param messageInfo the message to be printed instructing the user to
-     * input
-     * @param messageErrorOutOfRange the message will be printed if the String
-     * parse is out of range
-     * @param messageErrorInvalidNumber the message will be printed if the
-     * String does not contain a parable integer
-     * @param min minimum limit value
-     * @param max maximum limit value
-     * @return the valid integer value scanner from the input
-     */
-    public static int getInt(
-            String messageInfo,
-            String messageErrorOutOfRange,
-            String messageErrorInvalidNumber,
-            int min, int max
-    ) {
-        do {
-            try {
-                System.out.print(messageInfo);
-                int number = Integer.parseInt(SCANNER.nextLine());
-                if (number >= min && number <= max) {
-                    return number;
-                }
-                System.out.println(messageErrorOutOfRange);
-            } catch (NumberFormatException e) {
-                System.out.println(messageErrorInvalidNumber);
-            }
-        } while (true);
-    }
-
     public static void main(String[] args) {
-        
+
         int numberOfArray = getInt("Enter number of array: ",
                 "Number must be integer", "Number of array", 0, Integer.MAX_VALUE);
-        MergeSort sorted = new MergeSort(numberOfArray);
+        MergeSort sort = new MergeSort(numberOfArray);
         System.out.print("Unsorted array: ");
-        sorted.displayArray();
-        sorted.sort();
+        sort.displayArray();
+        sort.sort(true);
         System.out.print("Sorted array: ");
-        sorted.displayArray();
+        sort.displayArray();
 
     }
 }
