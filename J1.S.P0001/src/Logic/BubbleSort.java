40ac9ef76475dfe203a239410e583103b5edf493
Quang2k3 <nguyen.quang6633@gmail.com>
2023-09-15 21:44:13 +0700

    update

--- J1.S.P0001/src/Logic/BubbleSort.java
+++ J1.S.P0001/src/Logic/BubbleSort.java
@@ -1,4 +1,4 @@
-package Validate;
+package Logic;
 
 import java.util.Random;
 
@@ -7,32 +7,40 @@ import java.util.Random;
  * @author Quang
  */
 public class BubbleSort {
-    
+
     int arr[];
-    
+
     public BubbleSort(int numberOfArray) {
         arr = new int[numberOfArray];
     }
-    
+
     public void generateRandomIntegerOfArray() {
         Random random = new Random();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(arr.length + 1);
         }
     }
-    
-    public void bubbleSort() {
+
+    public void bubbleSort(boolean check) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if (check) {
+                    if (arr[j] > arr[j + 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                } else {
+                    if (arr[j] < arr[j + 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
                 }
             }
         }
     }
-    
+
     public void displayArray() {
         System.out.print("[");
         for (int i = 0; i < arr.length; i++) {
@@ -44,4 +52,5 @@ public class BubbleSort {
         System.out.print("]");
         System.out.println();
     }
+
 }
